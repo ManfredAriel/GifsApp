d8e94cdd70f178891e815c8b6584a2acb65bd748
Manfred <mmartinez@palcort.com>
2022-12-03 13:42:18 -0500

    feat: viewChild. - Esto nos permite adquirir datos de la interface de usuario.

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
 
-  constructor() { }
+export class BusquedaComponent {
 
-  ngOnInit(): void {
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  buscar(): void {
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor)
+    this.txtBuscar.nativeElement.value = '';
   }
+
+
 
 }
