5dbd1751e419990956fd24e8bdd6f6c8c445d0ce
Manfred <mmartinez@palcort.com>
2022-12-03 14:46:14 -0500

    feat: Se crean peticiones http con el modulo http

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,20 +7,27 @@ import { Injectable } from '@angular/core';
 
 export class GifsService {
 
+  private apiKey: string = 'P6PzPLwUrntjWvv0L60yalg88RkE00gw'
   private _historial: string[] = [];
 
   get historial() {
     return [...this._historial]
   }
 
+  constructor(private http: HttpClient) { }
+
   buscatGifs(query: string) {
     query = query.trim().toLocaleLowerCase();
-    
+
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10)
     }
 
-    console.log(this._historial);
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=P6PzPLwUrntjWvv0L60yalg88RkE00gw&q=dbz&limit=10')
+      .subscribe((resp: any) => {
+        console.log(resp.data);
+
+      })
   }
 }
